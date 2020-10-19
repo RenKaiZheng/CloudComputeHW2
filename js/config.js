b3bcc6936922a3d6e7904cf9e3c29f4741c71e19
RenkaiZheng <b06902127@ntu.edu.tw>
2020-10-19 23:38:11 +0800

    init copied src

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-east-2_vfO4sQSey', 
+        userPoolClientId: '2omjfpdeqtcuht1ne8bk1ib4p0', 
+        region: 'us-east-2'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://lg0g5axnz9.execute-api.us-east-2.amazonaws.com/prod'
     }
 };
